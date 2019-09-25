9f84e6ae9499be4435622acd5ea703780f8f6f40
asamlhk <asa_hp_choi@manulife.com>
2019-09-25 17:22:21 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Angular';
+ 
   value = "123"
-  nums = '០១២៣៤៥៦៧៨៩';
-
-  update() {
-    let final = "";
-
-    for (var i = 0; i <= this.value.length; i++) {
-      let char = this.value.charAt(i - 1);
-      let n = this.nums.indexOf(char);
-      n > 0 ? final += n : final += char;
-    }
-    this.value = final;
-  }
+ 
 }
