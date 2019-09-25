8f29cae8203fc2ae5e0eb96741d4f0657e96f67d
asamlhk <asa_hp_choi@manulife.com>
2019-09-25 17:21:10 +0800

    2 files modified, 2 added

---
+++ src/app/mdigit.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appMdigit]'
+})
+export class MdigitDirective {
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('keyup') onMouseEnter() {
+
+    this.el.nativeElement.value = this.update(this.el.nativeElement.value);
+  }
+
+  private update(value) {
+    let final = "";
+    const nums = '០១២៣៤៥៦៧៨៩';
+
+    for (var i = 0; i <= value.length; i++) {
+      let char = value.charAt(i - 1);
+      let n = nums.indexOf(char);
+      n > 0 ? final += n : final += char;
+    }
+    return final;
+  }
+
+}
